284df16a139c3fcbdc33f35be38b1cec6b015338
april.3005 <aiperi.kamaldinova@gmail.com>
2019-12-07 22:19:58 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList A = new ArrayList();
+        ArrayList B = new ArrayList();
+        ArrayList C = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        A.add(sc.nextLine());
+        A.add(sc.nextLine());
+        A.add(sc.nextLine());
+        A.add(sc.nextLine());
+        A.add(sc.nextLine());
+
+        System.out.println(A);
+
+        B.add(sc.nextLine());
+        B.add(sc.nextLine());
+        B.add(sc.nextLine());
+        B.add(sc.nextLine());
+        B.add(sc.nextLine());
+
+        System.out.println(B);
+
+        C.add(A.get(0));
+        C.add(B.get(4));
+        C.add(A.get(1));
+        C.add(B.get(3));
+        C.add(A.get(2));
+        C.add(B.get(2));
+        C.add(A.get(3));
+        C.add(B.get(1));
+        C.add(A.get(4));
+        C.add(B.get(0));
+
+
+        System.out.println(C);
+
+Collections.sort(C, new Comparator<String>() {
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+});
+
+        System.out.println("Сортированный список С");
+        System.out.println(C);
+
+    }
+}
